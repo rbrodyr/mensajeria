410fd91bb77bd17c279dbf0fcdc9fefecb7cbcf0
mgallachp <mgallachp@PC-67-00>
2021-10-19 13:13:50 +0200

    Creación proyecto

---
+++ borrarBasico/src/borrarBasico/interfaz1.java
@@ -0,0 +1,43 @@
+package borrarBasico;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class interfaz1 {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					interfaz1 window = new interfaz1();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public interfaz1() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
